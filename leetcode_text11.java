0aafab01c831c825134e1739abcdb23b8e53108c
lvaeieo <3470395233@qq.com>
2024-03-06 19:55:51 +0800

    2024-3-6-leetcode-day11

---
+++ leetcode_text11.java
@@ -0,0 +1,38 @@
+public class leetcode_text11 {
+}
+class Solution089 {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+}
+//给定一个整数 num，计算所有小于等于 num 的非负整数中数字 1 出现的个数。
+class Solution162 {
+    public int digitOneInNumber(int num) {
+        int digit = 1, res = 0;
+        int high = num / 10, cur = num % 10, low = 0;
+        while(high != 0 || cur != 0) {
+            if(cur == 0) res += high * digit;
+            else if(cur == 1) res += high * digit + low + 1;
+            else res += (high + 1) * digit;
+            low += cur * digit;
+            cur = high % 10;
+            high /= 10;
+            digit *= 10;
+        }
+        return res;
+    }
+}
+
